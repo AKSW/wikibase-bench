43ae883ff7bf14872e839a6a64769fe3afe36ce0
Daniel Hernández <daniel@degu.cl>
2016-03-29 12:14:40 -0300

    Generate queries and read a quins file

--- lib/wikidata.rb
+++ lib/wikidata.rb
@@ -8,23 +8,23 @@ include ERB::Util
 
 module Wikidata
 
-  # Run a query from a file.
-  def self.run_query(endpoint, file, timeout)
-    # Setup http
-    http = Net::HTTP.new(URI.parse(endpoint).host, URI.parse(endpoint).port)
-    http.open_timeout = 60
-    http.read_timeout = timeout
-
-    # Setup query
+  # Reading a query from a file.
+  def self.read_query(file_name)
     query = ''
     File.new(file).each_line do |line|
       line.sub!(/#.*$/,'').strip!
       query << (line + ' ') unless line.empty?
     end
     query.strip!
-
-    # Execute the query
+  end
+  
+  # Run a query.
+  def self.run_query(endpoint, query, timeout)
+    http = Net::HTTP.new(URI.parse(endpoint).host, URI.parse(endpoint).port)
+    http.open_timeout = 60
+    http.read_timeout = timeout
     url = "#{endpoint}?query=#{url_encode(query)}"
+    
     begin
       t1 = Time.now
       resp = http.get(URI(url), {'Accept'=>'application/json'})
@@ -42,5 +42,69 @@ module Wikidata
       return doc['results']['bindings'].size
     end
   end
-  
+
+  # Generate a naryrel query from a quin
+  def self.generate_query_naryrel(mask, quin, limit)
+    query = ''
+
+    # Prefixes
+    if mask[1] == '0' or mask[3] == '0'
+      query += 'PREFIX wikibase: <http://wikiba.se/ontology-beta#> '
+    end
+    if mask[0] == '1' or mask[2] == '1' or mask[4] == '1'
+      query += 'PREFIX wd: <http://www.wikidata.org/entity/> '
+    end
+    if mask[1] == '1' or mask[3] == '1'
+      query += 'PREFIX p: <http://www.wikidata.org/prop/> '
+    end
+    if mask[1] == '1'
+      query += 'PREFIX ps: <http://www.wikidata.org/prop/statement/> '
+    end
+    
+    # Select clause
+    query += 'SELECT '
+    if mask == '11111'
+      query += '* '
+    else
+      query += '?s '  if mask[0] == '0'
+      query += '?p '  if mask[1] == '0'
+      query += '?o '  if mask[2] == '0'
+      query += '?q '  if mask[3] == '0'
+      query += '?qo ' if mask[4] == '0'
+    end
+    query += '{ '
+
+    # Pattern variables
+    s  = ((mask[0] == '0') ? '?s'  : "wd:#{quin[0]}")
+    p  = ((mask[1] == '0') ? '?p'  : "p:#{quin[1]}")
+    ps = ((mask[1] == '0') ? '?ps' : "ps:#{quin[1]}")
+    o  = ((mask[2] == '0') ? '?o'  : "wd:#{quin[2]}")
+    q  = ((mask[3] == '0') ? '?q'  : "p:#{quin[3]}")
+    qo = ((mask[4] == '0') ? '?qo' : "wd:#{quin[4]}")
+    query += "#{s} #{p} [ #{ps} #{o} ; #{q} #{qo} ] . "
+
+    # Aditional restrictions
+    query += "#{p} wikibase:propertyValue #{ps} . " if mask[1] == '0'
+    query += "#{q} a wikibase:Property . " if mask[3] == '0'
+
+    # Limit
+    query += "} LIMIT #{limit}"
+  end
+
+  # Generate a query from a quin
+  def self.query(mode, mask, quin, limit)
+    case mode
+    when :naryrel
+      generate_query_naryrel(mask, quin, limit)
+    end
+  end
+
+  # Reads a quins file.
+  def self.read_quins(file_name)
+    quins = []
+    File.new(file_name, 'r').each do |line|
+      quins << line.split(' ').map{|x| x.strip }
+    end
+    quins
+  end
 end
