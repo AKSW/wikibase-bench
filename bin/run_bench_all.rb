d97684d6aec4ed4c0650583b7b6941d6197462f0
Daniel Hernández <daniel@degu.cl>
2016-03-31 18:00:21 -0300

    Reversing masks

--- bin/run_bench_all.rb
+++ bin/run_bench_all.rb
@@ -15,31 +15,33 @@ log_csv  = File.new('run_bench_all_log.csv', 'a')
 
 quins = Wikidata.read_quins(File.join('data', "quins-all.csv"))
 endpoint = "http://localhost:8000/sparql/"
-schema = :ngraphs # :naryrel
-(1..31).each do |i|
-  mask = "%05b" % i
-  puts "Running bench for #{mask} (#{schema})"
 
-  # Start server
-  puts "Starting server #{schema} #{mask} #{1+i%2}"
-  STDOUT.flush
-  system "cd /usr/local/virtuoso-opensource/var/lib/virtuoso/db-#{schema}-#{1+i%2} && virtuoso-t"
-  sleep 180
+[:naryrel, :ngraphs].each do |schema|
+  (1..31).each do |i|
+    mask = ("%05b" % i).reverse
+    puts "Running bench for #{mask} (#{schema})"
 
-  start = 500*(i-1)
-  (start...(start+QUERIES)).each do |j|
-    puts "Executing query #{schema} #{mask} #{j}"
-    query = Wikidata::generate_query(schema, mask, quins[j], LIMIT)
-    result = Wikidata::run_query(endpoint, query, 60)
-    array = [schema,mask,j,result[:time],nil,result[:status]]
-    array[4] = Wikidata::solutions(result) if result[:status] == '200'
-    log_csv.puts array.to_csv
-    log_csv.flush
+    # Start server
+    puts "Starting server #{schema} #{mask} #{1+i%2}"
+    STDOUT.flush
+    system "cd /usr/local/virtuoso-opensource/var/lib/virtuoso/db-#{schema}-#{1+i%2} && virtuoso-t"
+    sleep 180
+
+    start = 500*(i-1)
+    (start...(start+QUERIES)).each do |j|
+      puts "Executing query #{schema} #{mask} #{j}"
+      query = Wikidata::generate_query(schema, mask, quins[j], LIMIT)
+      result = Wikidata::run_query(endpoint, query, 60)
+      array = [schema,mask,j,result[:time],nil,result[:status]]
+      array[4] = Wikidata::solutions(result) if result[:status] == '200'
+      log_csv.puts array.to_csv
+      log_csv.flush
+    end
+    
+    # Stop server
+    puts 'Stoping server'
+    STDOUT.flush
+    system 'pidof virtuoso-t | xargs kill'
+    sleep 60
   end
-  
-  # Stop server
-  puts 'Stoping server'
-  STDOUT.flush
-  system 'pidof virtuoso-t | xargs kill'
-  sleep 60
 end
