c3dec2a1a15e91bab46d041884d5ea8783188e35
Daniel Hernández <daniel@degu.cl>
2016-04-06 16:43:08 -0300

    Adding all counted lines

--- bin/count_lines.rb
+++ bin/count_lines.rb
@@ -9,8 +9,9 @@ lines = 0
 
 Dir["#{ARGV[0]}/*.gz"].sort.each do |file_name|
   puts "Counting in #{file_name}"
-  n = `gunzip -c #{file_name} | wc -l`
+  n = `gunzip -c #{file_name} | wc -l`.strip.to_i
   puts "#{n} lines"
+  lines += n
 end
 
 puts "Total: #{lines} lines"
