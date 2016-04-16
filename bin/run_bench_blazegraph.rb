2074324dc03d045f24d1bb4d1b0101b398833ce0
Daniel Hernández <daniel@degu.cl>
2016-04-16 18:11:52 -0300

    Running blazegraph benchmarks

---
+++ bin/run_bench_blazegraph.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require "#{File.dirname(File.dirname(File.expand_path(__FILE__)))}/lib/wikidata.rb"
+
+if ARGV.size != 3
+  print "Usage: run_bench_all.rb <limit> <queries> <schema>"
+  exit 1
+end
+
+LIMIT   = ARGV[0].to_i
+QUERIES = ARGV[1].to_i
+
+case ARGV[2]
+when 'all'
+  schemas = [:naryrel, :ngraphs, :sgprop, :stdreif, :onaryrel, :ongraphs, :osgprop, :ostdreif]
+when 'without-optional'
+  schemas = [:naryrel, :ngraphs, :sgprop, :stdreif]
+when 'with-optional'
+  schemas = [:onaryrel, :ongraphs, :osgprop, :ostdreif]
+else
+  schemas = [ARGV[2].to_sym]
+end
+
+STDOUT.sync = true
+
+log_csv  = File.new('run_bench_all_log.csv', 'a')
+
+quins = Wikidata.read_quins(File.join('data', "quins-all.csv"))
+endpoint = "http://localhost:8000/sparql/"
+
+schemas.each do |schema|
+  (1..31).each do |i|
+    mask = ("%05b" % i).reverse
+    puts "Running bench for #{mask} (#{schema})"
+
+    # Start server
+    puts "Starting server #{schema} #{mask} #{1+i%2}"
+    server = Wikidata::BlazeGraph.new(schema, 1+i%2)
+    server.start
+    sleep 180
+
+    builder = QueryBuilder.new schema, mask
+
+    start = 500*(i-1)
+    (start...(start+QUERIES)).each do |j|
+      puts "Executing query #{schema} #{mask} #{j}"
+      query = builder.build quins[j], LIMIT
+      result = query.run endpoint, 60
+      array = [schema, mask, j, result[:time], nil, result[:status]]
+      array[4] = Query.solutions(result) if result[:status] == '200'
+      log_csv.puts array.to_csv
+      log_csv.flush
+    end
+
+    # Stop server
+    puts 'Stoping server'
+    server.stop
+    sleep 60
+  end
+end
--- lib/wikidata.rb
+++ lib/wikidata.rb
@@ -6,172 +6,193 @@ include ERB::Util
 
 module Wikidata
 
-  # Reading a query from a file.
-  def self.read_query(file_name)
-    query = ''
-    File.new(file).each_line do |line|
-      line.sub!(/#.*$/,'').strip!
-      query << (line + ' ') unless line.empty?
+  class Query
+
+    attr_reader :query, :result
+
+    def initialize(query)
+      @query = query
     end
-    query.strip!
-  end
-  
-  # Run a query.
-  def self.run_query(endpoint, query, timeout)
-    http = Net::HTTP.new(URI.parse(endpoint).host, URI.parse(endpoint).port)
-    http.open_timeout = 60
-    http.read_timeout = timeout
-    url = "#{endpoint}?query=#{url_encode(query)}"
-    
-    t1 = Time.now
-    begin
-      resp = http.get(URI(url), {'Accept'=>'application/json'})
-      t2 = Time.now
-      result = {time: t2-t1, body: resp.body, status: resp.code}
-    rescue RuntimeError => e
-      t2 = Time.now
-      result = {time: t2-t1, status: 'timeout', error: e}
+
+    # Create a new query from a file
+    def self.open(file_name)
+      query = ''
+      File.new(file).each_line do |line|
+        line.sub!(/#.*$/,'').strip!
+        query << (line + ' ') unless line.empty?
+      end
+      self.new(query.strip)
     end
+
+    # Run a query against an endpoint.
+    def run(endpoint, timeout)
+      http = Net::HTTP.new(URI.parse(endpoint).host, URI.parse(endpoint).port)
+      http.open_timeout = 60
+      http.read_timeout = timeout
+      url = "#{endpoint}?query=#{url_encode(@query)}"
+
+      t1 = Time.now
+      begin
+        resp = http.get(URI(url), {'Accept'=>'application/json'})
+        t2 = Time.now
+        result = {time: t2-t1, body: resp.body, status: resp.code}
+      rescue RuntimeError => e
+        t2 = Time.now
+        result = {time: t2-t1, status: 'timeout', error: e}
+      end
+    end
+
+    # Count solutions of a query
+    def self.solutions(results)
+      if results[:status].to_i == 200
+        doc = JSON.parse(results[:body])
+        doc['results']['bindings'].size
+      else
+        nil
+      end
+    end
+
   end
 
-  # Count results of a query
-  def self.solutions(results)
-    if results[:status].to_i == 200
-      doc = JSON.parse(results[:body])
-      return doc['results']['bindings'].size
-    end
-  end
+  class QueryBuilder
 
-  def self.prefixes(schema, mask)
-    query = ''
-    if [:naryrel, :onaryrel, :ngraphs, :ongraphs].include? schema
-      if mask[1] == '0' or mask[3] == '0'
-        query += 'PREFIX wikibase: <http://wikiba.se/ontology-beta#> '
-      end
+    def initilize(schema, mask)
+      @schema = schema
+      @mask   = mask
     end
-    if [:naryrel, :onaryrel, :ngraphs, :ongraphs, :sgprop, :osgprop, :stdreif, :ostdreif].include? schema
-      if mask[0] == '1' or mask[2] == '1' or mask[4] == '1'
-        query += 'PREFIX wd: <http://www.wikidata.org/entity/> '
-      end
-      if mask[1] == '1' or mask[3] == '1'
-        query += 'PREFIX p: <http://www.wikidata.org/prop/> '
-      end
-    end
-    if [:sgprop, :osgprop, :stdreif, :ostdreif].include? schema
-      query += 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>'
-      if mask[3] == '0'
-        query += 'PREFIX wikibase: <http://wikiba.se/ontology-beta#> '
-      end
-    end
-    if [:naryrel, :onaryrel].include? schema
-      if mask[1] == '1'
-	query += 'PREFIX ps: <http://www.wikidata.org/prop/statement/> '
-      end
-    end
-    query
-  end
-  
-  def self.select_variables(mask)
-    query = ''
-    if mask == '11111'
-      query += '* '
-    else
-      query += '?s '  if mask[0] == '0'
-      query += '?p '  if mask[1] == '0'
-      query += '?o '  if mask[2] == '0'
-      query += '?q '  if mask[3] == '0'
-      query += '?qo ' if mask[4] == '0'
-    end
-    query
-  end
 
-  def self.query_symbols(mask, quin)
-    symbols = {}
-    symbols[:s]  = ((mask[0] == '0') ? '?s'  : "wd:#{quin[0]}")
-    symbols[:p]  = ((mask[1] == '0') ? '?p'  : "p:#{quin[1]}")
-    symbols[:ps] = ((mask[1] == '0') ? '?ps' : "ps:#{quin[1]}")
-    symbols[:o]  = ((mask[2] == '0') ? '?o'  : "wd:#{quin[2]}")
-    symbols[:q]  = ((mask[3] == '0') ? '?q'  : "p:#{quin[3]}")
-    symbols[:qo] = ((mask[4] == '0') ? '?qo' : "wd:#{quin[4]}")
-    symbols
-  end
-  
-  # Generate a graph pattern.
-  def self.graph_pattern(schema, mask, quin)
-    s = query_symbols(mask, quin)
-    query = ''
-    case schema
-    when :naryrel
-      query += "#{s[:s]} #{s[:p]} ?c . ?c #{s[:ps]} #{s[:o]} ; #{s[:q]} #{s[:qo]} . "
-      query += "#{s[:p]} wikibase:propertyValue #{s[:ps]} . " if mask[1] == '0'
-      query += "#{s[:q]} a wikibase:Property . " if mask[3] == '0'
-    when :onaryrel
-      if mask[3] == '1' or mask[4] == '1'
-        query += self.graph_pattern(:naryrel, mask, quin)
-      else
-        query += "{ #{s[:s]} #{s[:p]} ?c . ?c #{s[:ps]} #{s[:o]} . "
-        query += "#{s[:p]} wikibase:propertyValue #{s[:ps]} . " if mask[1] == '0'
-        query += "} OPTIONAL { "
-        query += "?c #{s[:q]} #{s[:qo]} . "
-        query += "#{s[:q]} a wikibase:Property . " if mask[3] == '0'
-        query += "}"
+    def prefixes
+      query = ''
+      if [:naryrel, :onaryrel, :ngraphs, :ongraphs].include? @schema
+        if @mask[1] == '0' or @mask[3] == '0'
+          query += 'PREFIX wikibase: <http://wikiba.se/ontology-beta#> '
+        end
       end
-    when :ngraphs
-      query += "GRAPH ?c { #{s[:s]} #{s[:p]} #{s[:o]} . ?c #{s[:q]} #{s[:qo]} } . "
-      query += "#{s[:p]} a wikibase:Property . " if mask[1] == '0'
-      query += "#{s[:q]} a wikibase:Property . " if mask[3] == '0'
-      query += "FILTER (#{s[:s]} != ?c) " if mask[0] == '0'
-    when :ongraphs
-      if mask[3] == '1' or mask[4] == '1'
-        query += self.graph_pattern(:ngraphs, mask, quin)
-      else
-        query += "{ GRAPH ?c { #{s[:s]} #{s[:p]} #{s[:o]} } . "
-        query += "#{s[:p]} a wikibase:Property . " if mask[1] == '0'
-        query += "FILTER (#{s[:s]} != ?c) " if mask[0] == '0'
-        query += "} OPTIONAL { "
-        query += "GRAPH ?c { ?c #{s[:q]} #{s[:qo]} } . "
-        query += "#{s[:q]} a wikibase:Property . " if mask[3] == '0'
-        query += "}"
+      if [:naryrel, :onaryrel, :ngraphs, :ongraphs, :sgprop, :osgprop, :stdreif, :ostdreif].include? @schema
+        if @mask[0] == '1' or @mask[2] == '1' or @mask[4] == '1'
+          query += 'PREFIX wd: <http://www.wikidata.org/entity/> '
+        end
+        if @mask[1] == '1' or @mask[3] == '1'
+          query += 'PREFIX p: <http://www.wikidata.org/prop/> '
+        end
       end
-    when :sgprop
-      query += "#{s[:s]} ?c #{s[:o]} . ?c rdf:singletonPropertyOf #{s[:p]} ; #{s[:q]} #{s[:qo]} . "
-      query += "#{s[:q]} a wikibase:Property . " if mask[3] == '0'
-    when :osgprop
-      if mask[3] == '1' or mask[4] == '1'
-        query += self.graph_pattern(:sgprop, mask, quin)
-      else
-        query += "{ #{s[:s]} ?c #{s[:o]} . ?c rdf:singletonPropertyOf #{s[:p]} "
-        query += "} OPTIONAL { "
-        query += "?c #{s[:q]} #{s[:qo]} . "
-        query += "#{s[:q]} a wikibase:Property " if mask[3] == '0'
-        query += "}"
+      if [:sgprop, :osgprop, :stdreif, :ostdreif].include? @schema
+        query += 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>'
+        if @mask[3] == '0'
+          query += 'PREFIX wikibase: <http://wikiba.se/ontology-beta#> '
+        end
       end
-    when :stdreif
-      query += "?c rdf:subject #{s[:s]} ; rdf:predicate #{s[:p]} ; rdf:object #{s[:o]} ; #{s[:q]} #{s[:qo]} . "
-      query += "#{s[:q]} a wikibase:Property . " if mask[3] == '0'
-    when :ostdreif
-      if mask[3] == '1' or mask[4] == '1'
-        query += self.graph_pattern(:stdreif, mask, quin)
-      else
-        query += "{ ?c rdf:subject #{s[:s]} ; rdf:predicate #{s[:p]} ; rdf:object #{s[:o]} "
-        query += "} OPTIONAL { "
-        query += "?c #{s[:q]} #{s[:qo]} . "
-        query += "#{s[:q]} a wikibase:Property " if mask[3] == '0'
-        query += "}"
+      if [:naryrel, :onaryrel].include? @schema
+        if @mask[1] == '1'
+          query += 'PREFIX ps: <http://www.wikidata.org/prop/statement/> '
+        end
       end
+      query
     end
-    query
-  end
-  
-  # Generate a query.
-  def self.generate_query(schema, mask, quin, limit)
-    query = <<-EOS
-    #{prefixes(schema, mask)}
-    SELECT #{select_variables(mask)} {
-      #{graph_pattern(schema, mask, quin)}
-    } LIMIT #{limit}
-    EOS
-    query.gsub(/\s+/,' ').strip
+
+    def select_variables
+      query = ''
+      if @mask == '11111'
+        query += '* '
+      else
+        query += '?s '  if @mask[0] == '0'
+        query += '?p '  if @mask[1] == '0'
+        query += '?o '  if @mask[2] == '0'
+        query += '?q '  if @mask[3] == '0'
+        query += '?qo ' if @mask[4] == '0'
+      end
+      query
+    end
+
+    def quin_values(quin)
+      symbols = {}
+      symbols[:s]  = ((@mask[0] == '0') ? '?s'  : "wd:#{quin[0]}")
+      symbols[:p]  = ((@mask[1] == '0') ? '?p'  : "p:#{quin[1]}")
+      symbols[:ps] = ((@mask[1] == '0') ? '?ps' : "ps:#{quin[1]}")
+      symbols[:o]  = ((@mask[2] == '0') ? '?o'  : "wd:#{quin[2]}")
+      symbols[:q]  = ((@mask[3] == '0') ? '?q'  : "p:#{quin[3]}")
+      symbols[:qo] = ((@mask[4] == '0') ? '?qo' : "wd:#{quin[4]}")
+      symbols
+    end
+
+    # Generate a graph pattern.
+    def graph_pattern(quin)
+      s = quin_values(quin)
+      query = ''
+      case @schema
+      when :naryrel
+        query += "#{s[:s]} #{s[:p]} ?c . ?c #{s[:ps]} #{s[:o]} ; #{s[:q]} #{s[:qo]} . "
+        query += "#{s[:p]} wikibase:propertyValue #{s[:ps]} . " if @mask[1] == '0'
+        query += "#{s[:q]} a wikibase:Property . " if @mask[3] == '0'
+      when :onaryrel
+        if @mask[3] == '1' or @mask[4] == '1'
+          query += self.graph_pattern(:naryrel, @mask, quin)
+        else
+          query += "{ #{s[:s]} #{s[:p]} ?c . ?c #{s[:ps]} #{s[:o]} . "
+          query += "#{s[:p]} wikibase:propertyValue #{s[:ps]} . " if @mask[1] == '0'
+          query += "} OPTIONAL { "
+          query += "?c #{s[:q]} #{s[:qo]} . "
+          query += "#{s[:q]} a wikibase:Property . " if @mask[3] == '0'
+          query += "}"
+        end
+      when :ngraphs
+        query += "GRAPH ?c { #{s[:s]} #{s[:p]} #{s[:o]} . ?c #{s[:q]} #{s[:qo]} } . "
+        query += "#{s[:p]} a wikibase:Property . " if @mask[1] == '0'
+        query += "#{s[:q]} a wikibase:Property . " if @mask[3] == '0'
+        query += "FILTER (#{s[:s]} != ?c) " if @mask[0] == '0'
+      when :ongraphs
+        if @mask[3] == '1' or @mask[4] == '1'
+          query += graph_pattern(:ngraphs, @mask, quin)
+        else
+          query += "{ GRAPH ?c { #{s[:s]} #{s[:p]} #{s[:o]} } . "
+          query += "#{s[:p]} a wikibase:Property . " if @mask[1] == '0'
+          query += "FILTER (#{s[:s]} != ?c) " if @mask[0] == '0'
+          query += "} OPTIONAL { "
+          query += "GRAPH ?c { ?c #{s[:q]} #{s[:qo]} } . "
+          query += "#{s[:q]} a wikibase:Property . " if @mask[3] == '0'
+          query += "}"
+        end
+      when :sgprop
+        query += "#{s[:s]} ?c #{s[:o]} . ?c rdf:singletonPropertyOf #{s[:p]} ; #{s[:q]} #{s[:qo]} . "
+        query += "#{s[:q]} a wikibase:Property . " if @mask[3] == '0'
+      when :osgprop
+        if @mask[3] == '1' or @mask[4] == '1'
+          query += graph_pattern(:sgprop, @mask, quin)
+        else
+          query += "{ #{s[:s]} ?c #{s[:o]} . ?c rdf:singletonPropertyOf #{s[:p]} "
+          query += "} OPTIONAL { "
+          query += "?c #{s[:q]} #{s[:qo]} . "
+          query += "#{s[:q]} a wikibase:Property " if @mask[3] == '0'
+          query += "}"
+        end
+      when :stdreif
+        query += "?c rdf:subject #{s[:s]} ; rdf:predicate #{s[:p]} ; rdf:object #{s[:o]} ; #{s[:q]} #{s[:qo]} . "
+        query += "#{s[:q]} a wikibase:Property . " if @mask[3] == '0'
+      when :ostdreif
+        if @mask[3] == '1' or @mask[4] == '1'
+          query += graph_pattern(:stdreif, @mask, quin)
+        else
+          query += "{ ?c rdf:subject #{s[:s]} ; rdf:predicate #{s[:p]} ; rdf:object #{s[:o]} "
+          query += "} OPTIONAL { "
+          query += "?c #{s[:q]} #{s[:qo]} . "
+          query += "#{s[:q]} a wikibase:Property " if @mask[3] == '0'
+          query += "}"
+        end
+      end
+      query
+    end
+
+    # Generate a query.
+    def build(quin, limit)
+      query = [
+        prefixes,
+        'SELECT', select_variables,
+        'WHERE {', graph_pattern(quin), '}',
+        'LIMIT', limit
+      ].join(' ')
+      Query.new query
+    end
+
   end
 
   # Reads a quins file.
@@ -182,4 +203,64 @@ module Wikidata
     end
     quins
   end
+
+  class Database
+
+    def initialize(schema, id=1)
+      @schema = schema
+      @id     = id
+      @home   = "db-#{dbschema}-#{id}"
+    end
+
+    def dbschema
+      case @schema
+      when :naryrel, :onaryrel
+        :naryrel
+      when :ngraphs, :ongraphs
+        :ngraphs
+      when :sgprop, :osgprop
+        :sgprop
+      when :stdreif, :ostdreif
+        :stdreif
+      end
+    end
+
+    def self.stop
+      system "pidof #{@app} | xargs kill"
+    end
+
+  end
+
+  class BlazeGraph < Database
+
+    attr_reader :home
+
+    def initialize(schema, id=1)
+      super
+      @home = File.join('dbfiles','blazegraph',@home)
+      @app  = 'blazegraph'
+    end
+
+    def properties
+      case @schema
+      when :ngraphs, :ongraphs
+        "quads.properties"
+      else
+        "triples.properties"
+      end
+    end
+
+    def self.start(schema, id=1)
+      fork do
+        Dir.chdir @home
+        $stdout.reopen("out.log", "w")
+        $stderr.reopen("err.log", "w")
+        exec(['java', @app],
+          '-Xmx6g', '-Dbigdata.propertyFile=server.properties',
+          '-jar', 'blazegraph.jar')
+      end
+    end
+
+  end
+
 end
