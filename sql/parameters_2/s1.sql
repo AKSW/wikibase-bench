8434b8c8c8d57ee72ee56c20db62b6330d11e82a
Daniel Hernández <daniel@degu.cl>
2016-04-23 17:37:40 -0300

    Fixing inserting data.

--- sql/parameters_2/s1.sql
+++ sql/parameters_2/s1.sql
@@ -1,5 +1,5 @@
 -- We create a table to store ids and quantities of claims.
-CREATE TABLE entities_claim_quantities (
+CREATE TABLE IF NOT EXISTS entities_claim_quantities (
   claims int,
   entity_id text,
   PRIMARY KEY (claims, entity_id)
@@ -7,15 +7,15 @@ CREATE TABLE entities_claim_quantities (
 
 -- We fill this table.
 INSERT INTO entities_claim_quantities (claims, entity_id)
-SELECT count(claim_id) AS c, entity_id
+SELECT count(claim_id), entity_id
 FROM claims
 WHERE valueitem IS NOT NULL
 GROUP BY entity_id
-HAVING c >= 4;
+HAVING count(claim_id) >= 4;
 
 -- We create another table to store 500 random entities ids of entities
 -- with six or more claims.
-CREATE TABLE some_entity_pairs (
+CREATE TABLE IF NOT EXISTS some_entity_pairs (
   entity_a_id text,
   entity_b_id text,
   claim_id text,
