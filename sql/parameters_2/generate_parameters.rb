94e9fc49a315d236a9c27609c792153d1d10ee9f
Daniel Hernández <daniel@degu.cl>
2016-04-23 20:28:44 -0300

    Generating parameters

---
+++ sql/parameters_2/generate_parameters.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'sequel'
+require 'set'
+
+DB = Sequel.connect('postgres://wikidata:wikidata@localhost/wikidata_enzo')
+
+puts 'Reading start items'
+
+START_ITEMS = DB[:entities_claim_quantities].
+              where('claims >= ?', 5).
+              select(:entity_id).
+              all.map{ |item| item[:entity_id] }
+
+puts "readed: #{START_ITEMS.size} items."
+
+class Chain
+
+  def initialize
+    @chain = [ item_node(START_ITEMS.sample) ]
+  end
+  
+  def grow
+    last = @chain.last
+    if last[:claims].size > 0
+      claim = last[:claims].sample
+      last[:next] = claim[:claim_id]
+      @chain << item_node(claim[:valueitem])
+      return true
+    else
+      return false
+    end
+  end
+
+  def item_node(entity_id)
+    claims = DB[:claims].
+             where(entity_id: entity_id).
+             exclude(valueitem: nil).
+             select(:claim_id, :property, :valueitem).all
+    {
+      entity_id: entity_id,
+      claims: claims
+    }
+  end
+
+  def size
+    @chain.size
+  end
+  
+end
+
+chain_set = []
+chain_set_size = 10
+chain_size = 3
+
+
+while chain_set.size < chain_set_size
+  chain = Chain.new
+  while chain.size < chain_size
+    break unless chain.grow
+  end
+  if chain.size == chain_size
+    chain_set << chain
+    print '+'
+  else
+    print '.'
+  end
+end
+
+puts ''
+
+  
+  
+
+
+
--- sql/parameters_2/s1.sql
+++ sql/parameters_2/s1.sql
@@ -12,31 +12,3 @@ FROM claims
 WHERE valueitem IS NOT NULL
 GROUP BY entity_id
 HAVING count(claim_id) >= 4;
-
--- We create another table to store 500 random entities ids of entities
--- with six or more claims.
-CREATE TABLE IF NOT EXISTS some_entity_pairs (
-  entity_a_id text,
-  entity_b_id text,
-  claim_id text,
-  property text
-);
-
--- We fill this second table.
-INSERT INTO some_entity_pairs (entity_a_id, entity_b_id, claim_id, property)
-SELECT
-  a.entity_id,
-  b.entity_id,
-  claims.claim_id,
-  claims.property
-FROM
-  entities_claim_quantities AS a,
-  entities_claim_quantities AS b,
-  claims
-WHERE
-  a.entity_id = claims.entity_id AND
-  b.entity_id = claims.valueitem AND
-  a.claims >= 5 AND
-  a.claims >= 4
-ORDER BY random()
-LIMIT 500;
