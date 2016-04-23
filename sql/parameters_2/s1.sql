262d469fa851b412aa3fc1f03b30fd96bf1cec64
Daniel Hernández <daniel@degu.cl>
2016-04-23 17:30:42 -0300

    Generate query parameters.

---
+++ sql/parameters_2/s1.sql
@@ -0,0 +1,42 @@
+-- We create a table to store ids and quantities of claims.
+CREATE TABLE entities_claim_quantities (
+  claims int,
+  entity_id text,
+  PRIMARY KEY (claims, entity_id)
+);
+
+-- We fill this table.
+INSERT INTO entities_claim_quantities (claims, entity_id)
+SELECT count(claim_id) AS c, entity_id
+FROM claims
+WHERE valueitem IS NOT NULL
+GROUP BY entity_id
+HAVING c >= 4;
+
+-- We create another table to store 500 random entities ids of entities
+-- with six or more claims.
+CREATE TABLE some_entity_pairs (
+  entity_a_id text,
+  entity_b_id text,
+  claim_id text,
+  property text
+);
+
+-- We fill this second table.
+INSERT INTO some_entity_pairs (entity_a_id, entity_b_id, claim_id, property)
+SELECT
+  a.entity_id,
+  b.entity_id,
+  claims.claim_id,
+  claims.property
+FROM
+  entities_claim_quantities AS a,
+  entities_claim_quantities AS b,
+  claims
+WHERE
+  a.entity_id = claims.entity_id AND
+  b.entity_id = claims.valueitem AND
+  a.claims >= 5 AND
+  a.claims >= 4
+￼ORDER BY random()
+LIMIT 500;
