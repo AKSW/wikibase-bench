ba6f24a71cec4576c8b7ae45a2346e2a07e73f63
Daniel Hernández <daniel@degu.cl>
2016-04-28 22:20:32 -0300

    Generate paths

--- sql/parameters_2/generate_path.rb
+++ sql/parameters_2/generate_path.rb
@@ -14,10 +14,32 @@ PATH_NUMBER = ARGV[1]
 DB = Sequel.connect('postgres://wikidata:wikidata@localhost/wikidata_enzo')
 
 paths = DB[:paths].
-  where(size: PATH_LENGTH).
+#  where(size: PATH_LENGTH).
   order(Sequel.lit('RANDOM()')).
   limit(PATH_NUMBER)
 
-paths.each do |path|
-  p path
+paths.each do |row|
+  nodes = row[:nodes][1...-1].split(',').reverse
+  path = []
+  while nodes.size > 0
+    node = {claims: []}
+    node_id = nodes.pop
+    claims = DB[:claims].where(entity_id: node_id).exclude(valueitem: nil).select(:claim_id, :property, :valueitem).all
+    if nodes.size > 0
+      valueitem = nodes.last
+      claim = DB[:claims].where(entity_id: node_id, valueitem: valueitem).select(:claim_id, :property).all.sample
+      node[:property] = claim[:property]
+      claims.reject!{|c| c[:claim_id] == claim[:claim_id]}.shuffle
+    end
+    rand(3).times do
+      claim = claims.pop
+      node[:claims] << [claim[:property], claim[:valueitem]]
+    end
+    rand(3).times do
+      claim = claims.pop
+      node[:claims] << [claim[:property], rand(2)]
+    end
+    path << node
+  end
+  puts path.to_json
 end
