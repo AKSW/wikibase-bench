43a45393badb4421d75fe663c4fa087ee5e804f1
Daniel Hernández <daniel@degu.cl>
2016-04-30 09:40:17 -0300

    Sending a single query to a server.

---
+++ bin/send_query_file.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require './lib/wikidata.rb'
+
+if ARGV.size != 2
+  puts "usage bin/send_query_file.rb <endpoint> <file>"
+  exit 1
+end
+
+class Server
+  attr_reader :endpoint
+  def initialize(endpoint)
+    @endpoint = endpoint
+  end
+  def url(query)
+    "#{@endpoint}?query=#{url_encode(query)}&analytic=true"
+  end
+end
+
+server = Server.new(ARGV[0])
+query = Wikidata::Query.open(ARGV[1])
+puts query
+puts
+result = query.run(server, 180)
+puts result[:body]
+puts
+puts result[:time]
+puts result[:status]
